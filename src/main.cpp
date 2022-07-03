c7c223d8d9c12b43756c7e3c7f59e7456ee7148f
Alexander Goussas <goussasalexander@gmail.com>
2022-07-03 15:43:59 -0500

    add option to execute program

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,95 @@
 #include <cstdio>
+#include <cstdlib>
 #include <cstring>
 #include <iostream>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 #include "cat.hpp"
 
+#define CAT_TMP_NAME "cat-out.mips"
+#define SPIM_EXE "/usr/bin/spim"
+
+/// Write the program to a temporary file and run it using SPIM.
+void
+execute_program(const std::string& program)
+{
+  FILE* fp = std::fopen(CAT_TMP_NAME, "w");
+
+  if (!fp)
+    {
+      std::perror("Failed to create temporary file");
+      std::exit(EXIT_FAILURE);
+    }
+
+  if (std::fputs(program.c_str(), fp) < 0)
+    {
+      std::perror("Failed to write program to temporary file");
+      std::exit(EXIT_FAILURE);
+    }
+
+  if (std::fclose(fp) == EOF)
+    {
+      std::perror("Failed to close file");
+      std::exit(EXIT_FAILURE);
+    }
+
+  pid_t pid = fork();
+  int pipefd[2];
+  if (pipe(pipefd) == -1)
+    {
+      perror("Failed to pipe");
+      std::exit(EXIT_FAILURE);
+    }
+
+  if (pid == 0) // Child
+    {
+      // Close read end
+      close(pipefd[0]);
+
+      // Set our write end of the pipe to output stdout
+      if (dup2(1, pipefd[1]) == -1)
+        {
+          std::perror("Failed to duplicate 1");
+          std::exit(EXIT_FAILURE);
+        }
+
+      char* const argv[] = {
+        // Para que no joda el compilador
+        (char*)SPIM_EXE,
+        (char*)"-f",
+        (char*)CAT_TMP_NAME,
+        (char*)NULL,
+      };
+
+      execvp(SPIM_EXE, argv);
+
+      perror("exec");
+      std::exit(EXIT_FAILURE);
+    }
+
+  // Close write end
+  close(pipefd[1]);
+
+  // Wait for child to exit
+  int wstatus;
+  if (waitpid(pid, &wstatus, 0) < 0)
+    {
+      std::perror("Failed to wait for child");
+      std::exit(EXIT_FAILURE);
+    }
+
+  // Read & output child stdout
+  char buf[100];
+  while (read(pipefd[0], buf, sizeof(buf)) > 0)
+    std::cout << buf;
+
+  // Delete the temporary file
+  std::remove(CAT_TMP_NAME);
+}
+
+/// Run a repl.
 void
 repl()
 {
@@ -33,6 +119,7 @@ main(int argc, char** argv)
   argv++;
 
   std::string filename{};
+  bool run = false;
 
   if (argc == 0)
     {
@@ -44,39 +131,43 @@ main(int argc, char** argv)
   for (; i < argc; i++)
     {
       if (!std::strcmp(*argv, "-o"))
-        {
-          fout = fopen(*++argv, "w");
-        }
+        fout = fopen(*++argv, "w");
       else if (!std::strcmp(*argv, "-"))
         {
           filename = "stdin";
           fin = stdin;
+          argv++;
+        }
+      else if (!std::strcmp(*argv, "--run"))
+        {
+          run = true;
+          argv++;
         }
       else
-        {
-          break;
-        }
+        break;
     }
 
-  auto nargs{ argc - i };
-  if (nargs > 0)
+  if (auto nargs{ argc - i }; nargs > 0)
     {
       filename = *argv;
-      fin = std::fopen(*argv, "r");
+      fin = std::fopen(filename.c_str(), "r");
     }
 
-  char line[200];
+  char line[4096];
   std::string program{};
 
-  while (std::fgets(line, sizeof line, fin) != nullptr)
-    {
-      program += line;
-    }
+  while (std::fgets(line, sizeof(line), fin) != NULL)
+    program += line;
 
-  std::fputs(cat::transpile(program, filename).c_str(), fout);
+  auto result{ cat::transpile(program, filename) };
 
-  fclose(fout);
-  fclose(fin);
+  if (!run)
+    std::fputs(result.c_str(), fout);
+  else
+    execute_program(result);
+
+  std::fclose(fout);
+  std::fclose(fin);
 
   return 0;
 }
--- src/mips_transpiler.cpp
+++ src/mips_transpiler.cpp
@@ -140,8 +140,7 @@ void
 MIPSTranspiler::undeclared_variable_error(ast::Identifier& identifier)
 {
   // TODO: attach spans to expression to have access to them here
-  m_diagnostics.emplace_back(identifier.token().line(), "Unbound variable " + identifier.name(),
-                             Span{ 0, 0 });
+  m_diagnostics.emplace_back(identifier.token().line(), "Unbound variable " + identifier.name(), Span{ 0, 0 });
   std::string hint{ "Maybe you forgot to declare the variable?\n\n" };
   hint += "\t let " + identifier.name() + " := ...";
   m_diagnostics.push_back({ identifier.token().line(), Diagnostic::Severity::HINT, hint, Span{ 0, 0 } });
